4cade7bae1db3c42b651c2f8b448b626ca44ec2e
Cleysond10 <cleysonduarte99@gmail.com>
2025-03-13 03:20:40 -0300

    refactor: .env configs and example dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env.development.local',
+      envFilePath: '.env',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
